3c7263ef97d3ed585eef78353113e5197b67c0a2
AlinaZah31 <alina_shuvi@yahoo.com>
2024-07-11 15:37:52 +0200

    Update script.js (buttons instead of grid)

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
+let prevOption = "X";
+let arrayX = "", array0 = "";
 let weHaveWinner = 0;
-letsPlay();
 
-function letsPlay() {
-    let arrayX = "", array0 = "";
-
-    for (let i = 1; i <= 9; ++i) {
-        let option = document.getElementById(i);
-        option.addEventListener("input", function(event){
-            if (option.innerHTML == 'x') {
-                arrayX += i; 
-                //console.log("arrayX: " + arrayX);
-                displayResults(arrayX, 'x');
-            } else if (option.innerHTML == '0') {
-                array0 += i; 
-                //console.log("array0: " + array0);
-                displayResults(array0, '0');
-            }
-        })
+function changeValue(idBtn) {   
+    let currentBtn = document.getElementById(idBtn);
+    if (prevOption == "X") {
+        currentBtn.innerHTML = "0";
+        currentBtn.style.backgroundColor = "green";
+        array0 += idBtn;
+        prevOption = "0";
+        displayResults(array0, '0');
+    } else if (prevOption == "0") {
+        currentBtn.innerHTML = "X";
+        currentBtn.style.backgroundColor = "blue";
+        arrayX += idBtn;
+        prevOption = "X";
+        displayResults(arrayX, 'x');
     }
     if (weHaveWinner == 0) {
         document.getElementById("Initial-text").innerHTML = "NO WINNER";
@@ -25,13 +24,13 @@ function letsPlay() {
 
 function displayResults(string, winnerValue) {
     if ((string.includes(1) && string.includes(2) && string.includes(3)) || 
-    (string.includes(4) && string.includes(5) && string.includes(6)) || 
-    (string.includes(7) && string.includes(8) && string.includes(9)) || 
-    (string.includes(1) && string.includes(4) && string.includes(7)) || 
-    (string.includes(2) && string.includes(5) && string.includes(8)) || 
-    (string.includes(3) && string.includes(6) && string.includes(9)) || 
-    (string.includes(1) && string.includes(5) && string.includes(9)) || 
-    (string.includes(3) && string.includes(5) && string.includes(7))) {
+        (string.includes(4) && string.includes(5) && string.includes(6)) || 
+        (string.includes(7) && string.includes(8) && string.includes(9)) || 
+        (string.includes(1) && string.includes(4) && string.includes(7)) || 
+        (string.includes(2) && string.includes(5) && string.includes(8)) || 
+        (string.includes(3) && string.includes(6) && string.includes(9)) || 
+        (string.includes(1) && string.includes(5) && string.includes(9)) || 
+        (string.includes(3) && string.includes(5) && string.includes(7))) {
         document.getElementById("Initial-text").innerHTML = "The winner is: " + winnerValue;
         weHaveWinner = 1;
     }
