1a6671549aa5de0f89f0e64faf39ae74251e0b05
AlinaZah31 <alina_shuvi@yahoo.com>
2024-07-25 00:29:26 +0200

    Update script.js (array of winning combinations)

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let prevOption = "X";
-let arrayX = "", array0 = "";
+currentPlayer = "X";
 let weHaveWinner = 0;
+winnerValues = [[1, 2, 3], [4, 5, 6], [7, 8, 9], 
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], 
+    [1, 5, 9], [3, 5, 7]];
 
 generateButtons();
 
@@ -12,43 +14,33 @@ function generateButtons() {
         button.id = i;
         document.body.appendChild(button);
         button.onclick = "this.disabled=true";
-        button.onclick = function() {changeValue(i); this.disabled=true;}
+        button.onclick = function() {letsPlay(i, currentPlayer); this.disabled=true;}
         if (i % 3 == 0 ) {
             document.body.appendChild(document.createElement("br"));
         }  
     } 
 }
 
-function changeValue(idBtn) {   
+function letsPlay(idBtn, currentGamer) {   
     let currentBtn = document.getElementById(idBtn);
-    if (prevOption == "X") {
-        currentBtn.innerHTML = "0";
-        currentBtn.style.backgroundColor = "green";
-        array0 += idBtn;
-        prevOption = "0";
-        displayResults(array0, '0');
-    } else if (prevOption == "0") {
-        currentBtn.innerHTML = "X";
-        currentBtn.style.backgroundColor = "blue";
-        arrayX += idBtn;
-        prevOption = "X";
-        displayResults(arrayX, 'x');
+    if (currentGamer === "X") {
+        currentPlayer = "0";
+    } else if (currentGamer === "0") {
+        currentPlayer = "X";
+    } 
+    currentBtn.innerHTML = currentGamer;
+    for (let i = 0; i < winnerValues.length; ++i) {
+        for (let j = 0; j < 3; ++j) {
+            if (idBtn === winnerValues[i][j]) {
+                winnerValues[i][j] = currentGamer;
+            }
+            if (winnerValues[i][0] == winnerValues[i][1] && winnerValues[i][0] == winnerValues[i][2]) {
+                document.getElementById("Initial-text").innerHTML = "The winner is: " + currentGamer;
+                weHaveWinner = 1;
+            }
+        }
     }
     if (weHaveWinner == 0) {
         document.getElementById("Initial-text").innerHTML = "NO WINNER";
-    }
-}
-
-function displayResults(string, winnerValue) {
-    if ((string.includes(1) && string.includes(2) && string.includes(3)) || 
-        (string.includes(4) && string.includes(5) && string.includes(6)) || 
-        (string.includes(7) && string.includes(8) && string.includes(9)) || 
-        (string.includes(1) && string.includes(4) && string.includes(7)) || 
-        (string.includes(2) && string.includes(5) && string.includes(8)) || 
-        (string.includes(3) && string.includes(6) && string.includes(9)) || 
-        (string.includes(1) && string.includes(5) && string.includes(9)) || 
-        (string.includes(3) && string.includes(5) && string.includes(7))) {
-        document.getElementById("Initial-text").innerHTML = "The winner is: " + winnerValue;
-        weHaveWinner = 1;
     }
 }
