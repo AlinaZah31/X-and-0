5c70845b4c3277bbb6557600c148e63c1a0abe82
AlinaZah31 <alina_shuvi@yahoo.com>
2024-07-26 23:00:06 +0200

    Update script.js (after winning comb, the cell cannot be marked anymore)

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 currentPlayer = "X";
-let weHaveWinner = 0;
-winnerValues = [[1, 2, 3], [4, 5, 6], [7, 8, 9], 
-    [1, 4, 7], [2, 5, 8], [3, 6, 9], 
-    [1, 5, 9], [3, 5, 7]];
+let weHaveWinner = 0;  
+winnerValues = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
 
 generateButtons();
 
@@ -12,7 +10,7 @@ function generateButtons() {
         button.type = "button";
         button.innerText = " - ";
         button.id = i;
-        document.body.appendChild(button);
+        document.body.appendChild(button);    
         button.onclick = "this.disabled=true";
         button.onclick = function() {letsPlay(i, currentPlayer); this.disabled=true;}
         if (i % 3 == 0 ) {
@@ -21,26 +19,28 @@ function generateButtons() {
     } 
 }
 
-function letsPlay(idBtn, currentGamer) {   
+function letsPlay(idBtn, currentGamer) { 
+    let weHaveWinner1 = 0;  
     let currentBtn = document.getElementById(idBtn);
     if (currentGamer === "X") {
         currentPlayer = "0";
     } else if (currentGamer === "0") {
         currentPlayer = "X";
     } 
-    currentBtn.innerHTML = currentGamer;
-    for (let i = 0; i < winnerValues.length; ++i) {
-        for (let j = 0; j < 3; ++j) {
-            if (idBtn === winnerValues[i][j]) {
-                winnerValues[i][j] = currentGamer;
-            }
-            if (winnerValues[i][0] == winnerValues[i][1] && winnerValues[i][0] == winnerValues[i][2]) {
-                document.getElementById("Initial-text").innerHTML = "The winner is: " + currentGamer;
-                weHaveWinner = 1;
+    if (weHaveWinner == 0) {
+        currentBtn.innerHTML = currentGamer;
+        document.getElementById("Initial-text").innerHTML = "NO WINNER";
+        for (let i = 0; i < winnerValues.length; ++i) {
+            for (let j = 0; j < 3; ++j) {
+                if (idBtn === winnerValues[i][j]) {
+                    winnerValues[i][j] = currentGamer;
+                }
+                if (winnerValues[i][0] == winnerValues[i][1] && winnerValues[i][0] == winnerValues[i][2]) {
+                    document.getElementById("Initial-text").innerHTML = "The winner is: " + currentGamer;
+                    weHaveWinner1 = 1;
+                }
             }
         }
-    }
-    if (weHaveWinner == 0) {
-        document.getElementById("Initial-text").innerHTML = "NO WINNER";
-    }
+        weHaveWinner = weHaveWinner1;
+    }    
 }
